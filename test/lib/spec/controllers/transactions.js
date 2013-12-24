da5a6e84da9049f09a45fe94de4b3240df8d369d
Max Claus Nunes <maxcnunes@gmail.com>
2013-12-24 15:57:04 -0200

    changed importer to receive file from post and created test for bad request

--- test/lib/spec/controllers/transactions.js
+++ test/lib/spec/controllers/transactions.js
@@ -2,10 +2,21 @@ var request = require('supertest'),
       app = require('../../../../server');
 
 describe('GET /api/transactions/import', function(){
-  it('respond with success', function(done){
+  it('responds with success', function(done){
     request(app)
-      .get('/api/transactions/import')
+      .post('/api/transactions/import')
+      .attach('file', __dirname + '/../../fixtures/extract.ofx')
       .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('responds with bad request when not send a file', function(done){
+    request(app)
+      .post('/api/transactions/import')
+      .expect(400)
       .end(function(err, res){
         if (err) return done(err);
         done();
