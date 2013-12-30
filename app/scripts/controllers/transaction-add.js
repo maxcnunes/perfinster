92a5119ae016b88399332a80d449c74f9ebf47e7
Max Claus Nunes <maxcnunes@gmail.com>
2013-12-30 19:56:28 -0200

    add cancel button

--- app/scripts/controllers/transaction-add.js
+++ app/scripts/controllers/transaction-add.js
@@ -2,8 +2,20 @@
 
 angular.module('perfinsterApp')
   .controller('TransactionAddCtrl', function ($scope, $http, $location, TransactionService) {
+    (function init () {
+      $scope.master = {};
+    })();
+
     $scope.save = function () {
       TransactionService.create($scope.transaction);
+      $location.path('/transactions');
+    };
+
+    $scope.reset = function() {
+      $scope.transaction = angular.copy($scope.master);
+    };
+
+    $scope.back = function () {
       $location.path('/transactions');
     };
   });
