53b60b4ead522e3ebade26bbfba50f947174a0d6
Max Claus Nunes <maxcnunes@gmail.com>
2013-12-30 22:51:58 -0200

    created page to import extract bank file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('perfinsterApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'angularFileUpload'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
@@ -19,6 +20,10 @@ angular.module('perfinsterApp', [
       .when('/transactions/add', {
         templateUrl: 'partials/transaction-add',
         controller: 'TransactionAddCtrl'
+      })
+      .when('/transactions/import', {
+        templateUrl: 'partials/transaction-import',
+        controller: 'TransactionImportCtrl'
       })
       .otherwise({
         redirectTo: '/'
